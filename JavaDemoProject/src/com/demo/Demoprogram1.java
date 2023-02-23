ee82d0ff933f0048f0021c07c9d0d723ac8051fe
Prajwal BR <prbr@BLR.ALLEGISINDIA.COM>
2023-02-23 15:40:14 +0530

    newfileadded

---
+++ JavaDemoProject/src/com/demo/Demoprogram1.java
@@ -0,0 +1,8 @@
+package com.demo;
+
+public class Demoprogram1 {
+public static void main (String[] args) {
+	System.out.println("First program");
+	
+}
+}
---
+++ JavaDemoProject/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author prbr
+ *
+ */
+module JavaDemoProject {
+}
